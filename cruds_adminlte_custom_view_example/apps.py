df7bd394fc25affa60eb7191b26228869bd66de5
emystein <emystein@gmail.com>
2018-10-11 17:43:59 -0300

    Initial commit

---
+++ cruds_adminlte_custom_view_example/apps.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class CrudsAdminlteCustomViewExampleConfig(AppConfig):
+    name = 'cruds_adminlte_custom_view_example'
